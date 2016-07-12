773c43d3dbf98fe47b246814ee549ba4df591e2d
Joe Ryan <joe@clanryan.us>
2016-07-11 22:17:00 -0400

    create pi_counter.rb for functionality

---
+++ pi_counter.rb
@@ -0,0 +1,26 @@
+require 'pi_piper'
+
+puts "Press the switch to get started"
+pin3 = PiPiper::Pin.new(:pin => 3, :direction => :out)
+pin4 = PiPiper::Pin.new(:pin => 4, :direction => :out)
+pin17 = PiPiper::Pin.new(:pin => 17, :direction => :out)
+pin27 = PiPiper::Pin.new(:pin => 27, :direction => :out)
+
+
+pin17.off
+pin27.off
+
+sum = 0
+
+PiPiper.watch :pin => 22, :trigger => :rising do |pin|
+  sum += 1
+  puts sum
+
+  # get single bits of sum
+  pin3.update_value(sum & 0b0001 == 0b0001)
+  pin4.update_value(sum & 0b0010 == 0b0010)
+  pin17.update_value(sum & 0b0100 == 0b0100)
+  pin27.update_value(sum & 0b1000 == 0b1000)
+end
+
+PiPiper.wait
